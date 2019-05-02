628ca8db3ffb4dce2e62fe2df651a7378f257455
jibakurei <evangelion0000@outlook.com>
2019-05-02 15:51:48 +0800

    “‘“

--- PO/start_motion_appium.py
+++ PO/start_motion_appium.py
@@ -4,9 +4,9 @@ import time
 
 class start_motion_appium:
     def start_motion(self):
-        # d = ('start D:\Genymotion\player.exe --vm-name "Custom Phone - 8.0 - API 26 - 768x1280"')
-        motion = r'.\Data\motion_start.bat'
-        os.popen(motion)
+        d = ('start D:\Genymotion\player.exe --vm-name "Custom Phone - 8.0 - API 26 - 768x1280"')
+        # motion = r'.\Data\motion_start.bat'
+        os.popen(d)
         time.sleep(20)
     def start_appium(self):
         a = ('start appium&')
